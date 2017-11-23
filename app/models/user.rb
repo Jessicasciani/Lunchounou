4d79e7caad7b6f5f90619e5999ac20c29be01e7b
Jessica Scianimanica <sciani.jessica@gmail.com>
2017-11-23 15:23:49 +0000

    design groups index

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :photo, presence: true
 
   has_many :memberships
   has_many :groups_joined, through: :memberships, source: :group
+  mount_uploader :photo, PhotoUploader
 
   def groups_created
     Group.where(creator_id: self.id)
